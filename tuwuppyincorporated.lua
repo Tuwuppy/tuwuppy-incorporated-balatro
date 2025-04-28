55987345b87247ebfc4724d7cee1ec1e49588066
tuwuppy <tuwuppy@gmail.com>
2025-04-28 18:51:14 -0400

    make jonkler work?

--- tuwuppyincorporated.lua
+++ tuwuppyincorporated.lua
@@ -4,3 +4,42 @@ SMODS.Atlas {
     px = 71,
     py = 95
 }
+
+SMODS.Joker {
+    key = 'tinc-test',
+    loc_txt = {
+        name = "Test Jonkler",
+        text = {
+            "{C:mult}+#1# {} Mult"
+        }
+    },
+    config = { extra = { mult = 4 } },
+    loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.mult } }
+	end,
+	-- Sets rarity. 1 common, 2 uncommon, 3 rare, 4 legendary.
+	rarity = 1,
+	-- Which atlas key to pull from.
+	atlas = 'Placeholder',
+	-- This card's position on the atlas, starting at {x=0,y=0} for the very top left.
+	pos = { x = 0, y = 0 },
+	-- Cost of card in shop.
+	cost = 2,
+	-- The functioning part of the joker, looks at context to decide what step of scoring the game is on, and then gives a 'return' value if something activates.
+	calculate = function(self, card, context)
+		-- Tests if context.joker_main == true.
+		-- joker_main is a SMODS specific thing, and is where the effects of jokers that just give +stuff in the joker area area triggered, like Joker giving +Mult, Cavendish giving XMult, and Bull giving +Chips.
+		if context.joker_main then
+			-- Tells the joker what to do. In this case, it pulls the value of mult from the config, and tells the joker to use that variable as the "mult_mod".
+			return {
+				mult_mod = card.ability.extra.mult,
+				-- This is a localize function. Localize looks through the localization files, and translates it. It ensures your mod is able to be translated. I've left it out in most cases for clarity reasons, but this one is required, because it has a variable.
+				-- This specifically looks in the localization table for the 'variable' category, specifically under 'v_dictionary' in 'localization/en-us.lua', and searches that table for 'a_mult', which is short for add mult.
+				-- In the localization file, a_mult = "+#1#". Like with loc_vars, the vars in this message variable replace the #1#.
+				message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
+				-- Without this, the mult will stil be added, but it'll just show as a blank red square that doesn't have any text.
+			}
+		end
+	end
+}
+}
