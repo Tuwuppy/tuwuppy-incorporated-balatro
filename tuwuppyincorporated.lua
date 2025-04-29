0b2f0623bf11cf2cfad12fa1edf164dcd9dfba6b
tuwuppy <nova.skowronek@gmail.com>
2025-04-28 20:15:37 -0400

    change test jonkler to do xmult shenanigans

--- tuwuppyincorporated.lua
+++ tuwuppyincorporated.lua
@@ -10,34 +10,27 @@ SMODS.Joker {
     loc_txt = {
         name = "Test Jonkler",
         text = {
-            "{C:mult}+#1# {} Mult"
+            "{X:mult,C:white}X#1#{} Mult, Increases by the amount of times you've played a hand each time you play a hand."
         }
     },
-    config = { extra = { mult = 4 } },
-    loc_vars = function(self, info_queue, card)
-		return { vars = { card.ability.extra.mult } }
+	atlas = "Placeholders",
+	rarity = 4,
+	cost = 5,
+	config = {extra={xmult=2,xmult_change=1}},
+	loc_vars = function(self, info_queue, card)
+		return {
+			vars = {
+				card.ability.extra.xmult,
+				card.ability.extra.xmult_change
+			}
+		}
 	end,
-	-- Sets rarity. 1 common, 2 uncommon, 3 rare, 4 legendary.
-	rarity = 1,
-	-- Which atlas key to pull from.
-	atlas = 'Placeholders',
-	-- This card's position on the atlas, starting at {x=0,y=0} for the very top left.
-	pos = { x = 0, y = 0 },
-	-- Cost of card in shop.
-	cost = 2,
-	-- The functioning part of the joker, looks at context to decide what step of scoring the game is on, and then gives a 'return' value if something activates.
-	calculate = function(self, card, context)
-		-- Tests if context.joker_main == true.
-		-- joker_main is a SMODS specific thing, and is where the effects of jokers that just give +stuff in the joker area area triggered, like Joker giving +Mult, Cavendish giving XMult, and Bull giving +Chips.
+    calculate = function(self, card, context)
 		if context.joker_main then
-			-- Tells the joker what to do. In this case, it pulls the value of mult from the config, and tells the joker to use that variable as the "mult_mod".
+			card.ability.extra.xmult = card.ability.extra.xmult + card.ability.extra.xmult_change
+			card.ability.extra.xmult_change = card.ability.extra.xmult_change + 1
 			return {
-				mult_mod = card.ability.extra.mult,
-				-- This is a localize function. Localize looks through the localization files, and translates it. It ensures your mod is able to be translated. I've left it out in most cases for clarity reasons, but this one is required, because it has a variable.
-				-- This specifically looks in the localization table for the 'variable' category, specifically under 'v_dictionary' in 'localization/en-us.lua', and searches that table for 'a_mult', which is short for add mult.
-				-- In the localization file, a_mult = "+#1#". Like with loc_vars, the vars in this message variable replace the #1#.
-				message = localize { type = 'variable', key = 'a_mult', vars = { card.ability.extra.mult } }
-				-- Without this, the mult will stil be added, but it'll just show as a blank red square that doesn't have any text.
+				xmult = card.ability.extra.xmult
 			}
 		end
 	end
