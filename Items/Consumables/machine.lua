9d758187cda317c5b02b75334c09b39c74cd8d80
tuwuppy <nova.skowronek@gmail.com>
2025-04-30 11:07:39 -0400

    reorganize code to be in less files

--- Items/Consumables/machine.lua
+++ Items/Consumables/machine.lua
@@ -1,5 +1,17 @@
+SMODS.ConsumableType {
+    key = "machine",
+    primary_colour = HEX("808080"),
+    secondary_colour = HEX("d3d3d3"),
+    loc_txt = {
+        name = "Machine",
+        collection = "Machine",
+        undiscovered = {
+            name = "Broken Machine",
+            text = {"Find this machine in game to fix it in the collection!"},
+        }
+    }
+}
 -- if you forget: outside of calculate you do "ease_chips(5)" rather than "chips = 5"
-
 SMODS.Consumable {
     key = "machinetest",
     set = "machine",
--- tuwuppyincorporated.lua
+++ tuwuppyincorporated.lua
@@ -5,14 +5,14 @@ SMODS.Atlas {
     py = 47
 }
 
+-- add rarities inside their own files
+-- so make the industry rarity inside the industry jokers file
 local filesToLoad = {
-	"ConsumableTypes/machine",
-	"Jokers/common",
-	"Consumables/machine"
+	"Items/Consumables/machine",
+	"Items/Jokers/common",
 	-- add the other stuff i make in here later
 	-- also make sure you list the files in the order you want to load them,
 	-- so probably specific categories in order from most to least common and then the next category of items
-	-- load stuff that is required by other stuff (rarities) before the other obnes
 }
 -- loads all files in the filestoload array thing so that you can actually use them in game
 -- could go the way ruby did and make this far more complicated to order it properly but i can just order the table so :shrug:
